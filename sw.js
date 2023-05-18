b7b96243bc69b7026434aef041509829259781a1
matty-zee <mzheng1108@gmail.com>
2023-05-18 12:29:48 -0700

    finishing explore section

--- sw.js
+++ sw.js
@@ -2,7 +2,15 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-7-starter';
-
+// CONSTANTS
+const RECIPE_URLS = [
+  'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
+  'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
+  'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
+  'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
+];
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
@@ -34,7 +42,21 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        if(response) {
+          return response
+        }
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  //            network response.        
+        return fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+
 });
